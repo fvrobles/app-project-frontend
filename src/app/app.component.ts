ca177e532dba8cc9dddcba92fdf1ff8dcaddc4b7
FernandoVR <fernando.vr.dev@gmail.com>
2024-05-28 12:12:45 +0000

    feat: call backend API to get title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'helloworld';
+export class AppComponent implements OnInit {
+  title: string | undefined;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.getTitleFromAPI();
+  }
+
+  getTitleFromAPI() {
+    this.http.get('http://localhost:8081/api/title')
+      .subscribe({
+        next: (response: any) => {
+          this.title = response.title;
+        },
+        error: (error: any) => {
+          this.title = 'Error';
+        }
+      });
+  }
 }
